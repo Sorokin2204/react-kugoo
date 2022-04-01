3ebead747022f768787100e69c68fa7d7f0d1b4f
Sorokin2204 <daniil.sorokin.228@gmail.com>
2022-04-01 16:47:28 +0300

    Update config

--- next.config.js
+++ next.config.js
@@ -1,39 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
     ignoreBuildErrors: true,
   },
   reactStrictMode: true,
   async rewrites() {
     return {
       fallback: [
-        // These rewrites are checked after both pages/public files
-        // and dynamic routes are checked
         {
           source: '/static/:path*',
-          destination: `http://localhost:5000/static/:path*`,
+          destination: `https://react-kugoo-server-n7kmd7ns8-sorokin2204.vercel.app/static/:path*`,
         },
         {
           source: '/static',
-          destination: `http://localhost:5000/static`,
+          destination: `https://react-kugoo-server-n7kmd7ns8-sorokin2204.vercel.app/static`,
         },
       ],
     };
   },
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: '/static/:path*',
-  //       destination: `http://localhost:5000/static/:path*`,
-  //       permanent: false,
-  //       basePath: false,
-  //     },
-  //   ];
-  // },
 };
 
 module.exports = nextConfig;
